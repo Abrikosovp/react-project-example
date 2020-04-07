cf90bed49d097128df3a1c4e987521055d396948
Pavel Abrikosov <abrikosovp@mail.ru>
2020-04-07 14:50:25 +0300

    timer deadline

--- src/chapter_3_original/js/script.js
+++ src/chapter_3_original/js/script.js
@@ -1,4 +1,3 @@
-
 window.addEventListener("DOMContentLoaded", function () {
     'use script';
 
@@ -36,4 +35,56 @@ window.addEventListener("DOMContentLoaded", function () {
         }
 
     });
+
+    // Timer
+
+    let deadline = '2020-06-06';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.now(),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
+
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            function zeroTime(time) {
+                if (time <= 9) {
+                    return '0-' + time;
+                }
+                return time;
+            }
+
+
+            hours.textContent = zeroTime(t.hours);
+            minutes.textContent = zeroTime(t.minutes);
+            seconds.textContent = zeroTime(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+
+    setClock('timer', deadline);
 });
